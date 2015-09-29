bd6b55b483243ef4582a9d863b3ef6f1744855b8
dinglihome <dingli@qq.com>
2015-09-29 17:48:07 +0800

    add PostHtml

--- Job.Crawler/JobBase.cs
+++ Job.Crawler/JobBase.cs
@@ -11,16 +11,17 @@ namespace Job.Crawler
 {
     public abstract class JobBase
     {
+        private Log4Help.Log4NetHelp _loger = Log4Help.LogFactory.GetLogger(typeof(JobBase));
         protected virtual string GetHtml(string url)
         {
-            var result = "";
+            var result = string.Empty;
             try
             {
-                var webRequest = (HttpWebRequest) WebRequest.Create(url);
+                var webRequest = (HttpWebRequest)WebRequest.Create(url);
                 webRequest.Method = "Get";
                 webRequest.Proxy = null;
-                webRequest.UserAgent ="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36";
-                using (var response = (HttpWebResponse) webRequest.GetResponse())
+                webRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36";
+                using (var response = (HttpWebResponse)webRequest.GetResponse())
                 using (var responseStream = response.GetResponseStream())
                 using (var sr = new StreamReader(responseStream, Encoding.UTF8))
                 {
@@ -30,6 +31,36 @@ namespace Job.Crawler
             }
             catch (Exception e)
             {
+                _loger.Error(e);
+            }
+            return result;
+        }
+
+        protected virtual string PostHtml(string url, string postdata)
+        {
+            var result = string.Empty;
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+                webRequest.Method = "Post";
+                webRequest.Proxy = null;
+                webRequest.ContentType = "application/x-www-form-urlencoded";    //模拟头
+                webRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36";
+                Stream stream = webRequest.GetRequestStream();
+                byte[] bytes = Encoding.UTF8.GetBytes(postdata);
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Close();
+                WebResponse webResponse = webRequest.GetResponse();
+                var streamReader = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
+                result = streamReader.ReadToEnd();
+                streamReader.Dispose();
+                streamReader.Close();
+                webResponse.Close();
+                webRequest.Abort();
+            }
+            catch (Exception e)
+            {
+                _loger.Error(e);
             }
             return result;
         }
